ce0042cd250f5bbf161ea4b0b8a7d75fc3d4493f
Geoff Tipton <geoff.tipton@kroger.com>
2017-02-01 10:43:46 -0500

    OSS-7 - Add unit test for PortMapper.java

---
+++ src/test/java/com/kroger/rp/util/PortMapperTest.java
@@ -0,0 +1,41 @@
+package com.kroger.rp.util;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class PortMapperTest {
+
+    private PortMapper portMapper;
+
+    @Before
+    public void init() {
+        portMapper = new PortMapper();
+    }
+
+    @Test
+    public void shouldConsistentlyAssignCorrectPortToSecureAndInsecureUrls() {
+        portMapper.initMapping();
+        int securePort = portMapper.getMappedPortForUrl("https://secureurl.com");
+        int inSecurePort = portMapper.getMappedPortForUrl("http://insecureurl.com");
+        assertNotEquals(securePort, inSecurePort);
+
+        IntStream.rangeClosed(1, 1000).forEach(i -> assertEquals(securePort, (int) portMapper.getMappedPortForUrl("https://" + i + ".com")));
+        IntStream.rangeClosed(1, 1000).forEach(i -> assertEquals(inSecurePort, (int) portMapper.getMappedPortForUrl("http://" + i + ".com")));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldThrowExceptionOnBadUrlFormat() {
+        portMapper.initMapping();
+        portMapper.getMappedPortForUrl("badurl.com");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldThrowExceptionOnUnitializedPortMappings() {
+        portMapper.getMappedPortForUrl("http://url.com");
+    }
+}
