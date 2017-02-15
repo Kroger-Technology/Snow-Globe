564d9aa6a8ee2b6c4c00618d21bf988ce6e31fee
Geoff Tipton <geoff.tipton@kroger.com>
2017-02-15 16:50:06 -0500

    Add Apache 2.0 license to java source files

--- src/test/java/com/kroger/rp/util/call/ResponseVerificationTest.java
+++ src/test/java/com/kroger/rp/util/call/ResponseVerificationTest.java
@@ -1,3 +1,21 @@
+/*
+ * Nginx Snow Globe
+ *
+ * Copyright 2017 The Kroger Co.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.kroger.rp.util.call;
 
 import org.apache.http.Header;
@@ -11,8 +29,6 @@ import org.mockito.Mock;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.mockito.Mockito.doCallRealMethod;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
